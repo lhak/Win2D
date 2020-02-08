0ccf6c95318d1d21110e280015b2aedba4a1d001
Arno Klenke <arno_klenke@outlook.com>
2020-02-08 13:40:26 +0100

    Remove outdated xaml islands sample

--- samples/XamlIslandSample/XamlIslandSample.Desktop/ThumbnailGenerator.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace ExampleGallery
-{
-    // This is used by ExplosionParticleSystem, we only need this flag here.
-    public static class ThumbnailGenerator
-    {
-        public static readonly bool IsDrawingThumbnail = false;
-    }
-}
